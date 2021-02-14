5a8a377be867c36001fe2c5e3582ae6e6bf31740
simonmorgenstern <simonmorgenstern2001@gmail.com>
2021-02-14 15:19:44 +0100

    Spielschleife hinzufuegen
    Menue aufrufen
    Eingabe abfragen
    falls Spieler spielen möchte erste Karte aufdecken
    und rein in die Olga

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include "kartendeck.h"
 #include "handkarten.h"
+#include "menue.h"
+void init_spiel();
+void spiel_schleife();
 enum {spieler, bot1, bot2, bot3};
 enum k_farbe{ blau, rot, gruen, gelb, schwarz };
 enum richtung{links, rechts};
+enum {start, spiel, spielende};
 // Statusvariablen Spiel
 Karte letzteKarte;
 int spiel_richtung = rechts;
@@ -11,11 +15,34 @@ int zieh_counter = 0;
 int wunschfarbe;
 int spieler_reihenfolge[4] = {spieler, bot1, bot2, bot3};
 int aktueller_spieler = spieler;
+int spielstatus = start;
 
 int main() {
+    if(spielstart() == 1){
+        init_spiel();
+        spiel_schleife();
+    }
+    return 0;
+}
+
+void spiel_schleife(){
+    letzteKarte = Kartenstapel[oberste_stapel_karte];
+    oberste_stapel_karte++;
+    printf("%d", oberste_stapel_karte);
+    spielstatus = spiel;
+    while(spielstatus == spiel){
+        break;
+        // Hier finden alle Spielzüge statt
+        // Prüfen ob einer gewonnen hat
+    }
+    printf("Spiel Ende");
+    // Spielende Funktion wird aufgerufen
+    // Abfrage ob erneut gespielt werden soll
+}
+
+void init_spiel(){
     mische_karten();
     hole_handkarten_speicher();
     teile_karten_aus();
-    zeige_handkarten_spieler();
-    return 0;
 }
+
